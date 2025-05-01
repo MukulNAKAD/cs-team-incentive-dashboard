adff343f1f9a00e514b82ea4afd1d277af6ce94f
Mukul Kashyap <111637469+MukulNAKAD@users.noreply.github.com>
2025-05-01 18:55:30 +0530

    Add API services index export file

---
+++ src/api/index.ts
@@ -0,0 +1,26 @@
+// Auth exports
+export * from './auth'
+
+// Employee exports
+export * from './employees'
+
+// Client exports
+export * from './clients'
+
+// Pod exports
+export * from './pods'
+
+// Revenue exports
+export * from './revenue'
+
+// Cost exports
+export * from './costs'
+
+// Target exports
+export * from './targets'
+
+// Incentive exports
+export * from './incentives'
+
+// Report exports
+export * from './reports'
